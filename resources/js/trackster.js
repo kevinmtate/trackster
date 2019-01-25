c9f1fa6fe2e02fe71449f5f4e9415d8e317e3aef
Kevin Tate <2kmtate@gmail.com>
2019-01-25 13:45:41 -0500

    grr commented code

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -19,38 +19,33 @@ function search() {
 	});
 }
 
-
-/*
-  Given an array of track data, create the HTML for a Bootstrap row for each.
-  Append each "row" to the container in the body to display all tracks. 
-*/
 Trackster.renderTracks = function(tracks) {
 	$('.tracklist').empty();
 
-	for (i = 0; i < tracks.length; i++) {
-		var track = tracks[i];
+	let i=1;
+	tracks.forEach((track) => {
 		var trackNumber = i+1;
+		// var trackInfo = Trackster.getTrackInfo(track.artist, track.name);
+		// var album = trackInfo.track.album.title;
+		// console.log(album);
 		var mediumAlbumArt = track.image[1]['#text'];
 		var listeners = parseInt(track.listeners).toLocaleString();
 
 		var htmlTrackRow = 
 		"<div class='row track'> \
-			<div class='col-sm-1 text-right'>"+trackNumber+"</div> \
+			<div class='col-sm-1 text-center'>"+trackNumber+"</div> \
 			<a class='col-sm-1 text-right' href="+track.url+" target='_blank'><i class='fa fa-play-circle-o fa-lg fa-2x'></i></a> \
-			<div class='col-sm-4'>"+track.name+"</div> \
-			<div class='col-sm-2'>"+track.artist+"</div> \
+			<div class='col-sm-3'>"+track.name+"</div> \
+			<div class='col-sm-3'>"+track.artist+"</div> \
 			<div class='col-sm-2'><img src="+mediumAlbumArt+" width='50px' alt='album art'></div> \
 			<div class='col-sm-2'>"+listeners+"</div> \
 		</div>";
+		// <div class='col-sm-3'>"+album+"</div> \
 
 		$('.tracklist').append(htmlTrackRow);
-	}
+	});
 };
 
-/*
-  Given a search term as a string, query the LastFM API.
-  Render the tracks given in the API query response.
-*/
 Trackster.searchTracksByTitle = function(title) {
 	$.ajax({
 		url: 'https://ws.audioscrobbler.com/2.0/?method=track.search&track='+title+'&api_key='+API_KEY+'&format=json',
@@ -59,5 +54,16 @@ Trackster.searchTracksByTitle = function(title) {
 			console.log(data);
 			Trackster.renderTracks(data.results.trackmatches.track);
 		}
-	})
+	});
 };
+
+// Trackster.getTrackInfo = function(artist, track) {
+// 	return $.ajax({
+// 		url: 'http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key='+API_KEY+'&artist='+artist+'&track='+track+'&format=json',
+// 		datatype: 'jsonp',
+// 		async: false,
+// 		// success: function(data) {
+// 		// 	return data;
+// 		// }
+// 	}).responseJSON;
+// };
